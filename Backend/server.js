a49a0a650f52a656e8c09e2d5dce8db52c6ad7a9
Luis Enereh <enerehluis6@gmail.com>
2019-10-29 15:56:11 +0000

    commit

--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,23 @@ const app = express()
 const port = 3000
 const path = require('path')
 const bodyParser = require("body-parser");
+const mongoose =  require ("mongoose");
+
+
+
+const mongoDB = 'mongodb+srv://DBLFE:luis@cluster0-47zs3.mongodb.net/test?retryWrites=true&w=majority'
+mongoose.connect(mongoDB, {useNewUrlParser:true});
+
+const Schema = mongoose.Schema;
+
+const movieSchema = new Schema ({
+    title: String,
+    year: String, 
+    poster: String 
+});
+
+const MovieModel = mongoose.model('movie',movieSchema);
+
 
 const cors = require('cors');
 app.use(cors());
@@ -42,36 +59,54 @@ res.sendFile(path.join(__dirname + '/index.html'))
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.get('/api/movies', (req, res,next) => {
+    /*const movies = [
+        {
+        "Title":"Avengers: Infinity War",
+        "Year":"2018",
+        "Poster":"https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
+        },
+        {
+        "Title":"Captain America: Civil War",
+        "Year":"2016",
+        "Poster":"https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
+        }
+        ]
+        res.status(200).json({
+            message: "Everything is good",
+            myMovies:movies
+        })*/
+    
+    console.log ("get request") 
+    MovieModel.find((err,data) =>{
+        res.json({movies:data});
+    })   
+    //res.send('myAPI')
+    })
+
 app.post('/api/movies', (req,res) =>{
     console.log('post Sucessfull');
     console.log(req.body)
     console.log(req.body.title);
     console.log(req.body.year);
     console.log(req.body.poster);
+
+
+    MovieModel.create({
+        title:req.body.title,
+        year:req.body.year,
+        poster:req.body.poster
+    });
+
+        res.json('data uploaded');
+
     })
-    
+ 
+
 app.post('/name', (req, res) => {
     console.log("post method");
     console.log(req.body.firstname);
     res.send('Hello ' + req.body.firstname + " " + req.body.lastname);
     })
     
-app.get('/api/movies', (req, res) => {
-const movies = [
-    {
-    "Title":"Avengers: Infinity War",
-    "Year":"2018",
-    "Poster":"https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
-    },
-    {
-    "Title":"Captain America: Civil War",
-    "Year":"2016",
-    "Poster":"https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
-    }
-    ]
-    res.status(200).json({
-        message: "Everything is good",
-        myMovies:movies
-    })
-//res.send('myAPI')
-})
+
--- src/app/read/read.component.ts
+++ src/app/read/read.component.ts
@@ -12,7 +12,7 @@ export class ReadComponent implements OnInit {
 
   ngOnInit() {
     this.movieService.GetMovieInformation().subscribe((data) => {
-      this.MyMovies = data.myMovies;
+      this.MyMovies = data.movies;
       console.log(this.MyMovies);
     })
   }
